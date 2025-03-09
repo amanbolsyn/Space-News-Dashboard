6cd8cdb22fa3b4d23d637d7c77fb0bcbf1e172ae
Aman Nurgozhiyev <anurgozhiyev@gmail.com>
2025-03-10 04:00:05 +0500

    modif: fixed column grid for article cards and pagination for retrieved articles

--- space-news-dashboard/js/data.js
+++ space-news-dashboard/js/data.js
@@ -4,34 +4,40 @@ async function fetchJSONData() {
     let filePath = './assets/data/data.json';
     return fetch(filePath)
 
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();  // Resolve the promise with the JSON data
-    })
-    .then((data) => {return data})
-    .catch(error => {
-        console.error('Failed to fetch data:', error);
-        return null;  // Return null if there’s an errore
-    });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();  // Resolve the promise with the JSON data
+        })
+        .then((data) => { return data })
+        .catch(error => {
+            console.error('Failed to fetch data:', error);
+            return null;  // Return null if there’s an errore
+        });
 }
 
 
-function RetrieveArticles(data){
-    const searchStr = document.getElementById("search-input").value;
-   let articles = {results:[]};
+function RetrieveArticles(data) {
+
+    const searchStr = document.getElementById("search-input").value.trim().toLowerCase();
+    let articles = { results: [] };
+
+    //search logic
+    if (searchStr !== " ") {
+        //finding matched article titles by searching from search input
+        for (let i = 0; i < data.results.length; i++) {
+            if (data.results[i].title.toLowerCase().search(searchStr) !== -1) {
+                articles.results.push(data.results[i])
+            }
+        }
+    }
 
 
-   //finding matched article titles by searching from search input
-   for(let i = 0; i<data.results.length; i++){
-       if(data.results[i].title.search(searchStr) !== -1){
-        articles.results.push(data.results[i])
-       }
-   }
-    
-   console.log(articles)
-   return articles
+
+
+
+    return articles
 }
 
 
